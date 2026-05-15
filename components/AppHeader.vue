af0a1f1f40cc95f84d2082ea863f6e7239fca956
Joannis Orlandos <joannis@orlandos.nl>
2026-05-16 06:09:17 +0900

    Update the website with "Learn" & a benchmark (#67)

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 const links = computed(() => [{
-  label: 'Features',
-  to: '/',
-  icon: 'i-heroicons-cube-transparent'
-}, {
   label: 'News',
   to: '/news',
   icon: 'i-heroicons-light-bulb'
 }, {
+  label: 'Features',
+  to: '/features',
+  icon: 'i-heroicons-cube-transparent'
+}, {
   label: 'Ecosystem',
   to: '/ecosystem',
   icon: 'i-heroicons-rectangle-group'
+}, {
+  label: 'Learn',
+  to: '/learn',
+  icon: 'i-heroicons-academic-cap'
 }, {
   label: 'Examples',
   to: '/examples',
---
+++ pages/features.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+const { data: page } = await useAsyncData('features', () => queryContent('/features').findOne())
+
+useSeoMeta({
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description
+})
+</script>
+
+<template>
+  <div>
+    <ULandingSection>
+      <ULandingHero
+        :title="page.hero.title"
+        :description="page.hero.description"
+      />
+    </ULandingSection>
+
+    <ULandingSection
+      :headline="page.concurrency.headline"
+      :title="page.concurrency.title"
+      :description="page.concurrency.description"
+    >
+      <UPageGrid>
+        <ULandingCard
+          v-for="(item, index) in page.concurrency.items"
+          :key="index"
+          v-bind="item"
+        />
+      </UPageGrid>
+    </ULandingSection>
+
+    <ULandingSection
+      :headline="page.performance.headline"
+      :title="page.performance.title"
+      :description="page.performance.description"
+    >
+      <UPageGrid>
+        <ULandingCard
+          v-for="(item, index) in page.performance.items"
+          :key="index"
+          v-bind="item"
+        />
+      </UPageGrid>
+    </ULandingSection>
+
+    <ULandingSection
+      :headline="page.dx.headline"
+      :title="page.dx.title"
+      :description="page.dx.description"
+    >
+      <UPageGrid>
+        <ULandingCard
+          v-for="(item, index) in page.dx.items"
+          :key="index"
+          v-bind="item"
+        />
+      </UPageGrid>
+    </ULandingSection>
+  </div>
+</template>
